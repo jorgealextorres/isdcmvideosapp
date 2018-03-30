b5131ef5727af96faf29bcde6f1e5cd680369d2e
Jorge Torres <jorgetorres@10.0.0.11>
2018-03-30 22:23:17 +0200

    Changing to embedded h2 database.

--- src/java/common/DatabaseConnection.java
+++ src/java/common/DatabaseConnection.java
@@ -10,23 +10,37 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.h2.jdbcx.JdbcDataSource;
+
 /**
  *
  * @author jorgetorres
  */
 public class DatabaseConnection {
-    private static String dbURL = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11227091";
-    private static String dbUser = "sql11227091";
-    private static String dbUserPassword = "Q1etReKkYK";
+    private static JdbcDataSource ds = null;
+            
+    private static final String dbDriver = "org.h2.Driver";
+    private static final String dbURL = "jdbc:h2:./videosDb";
+    private static final String dbUser = "userdb";
+    private static final String dbUserPassword = "123456";
     // jdbc Connection
     private static Connection conn = null;
     
     private static void createConnection() throws ClassNotFoundException, InstantiationException, SQLException, IllegalAccessException
     {
+        /*
         //Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
         Class.forName("com.mysql.jdbc.Driver").newInstance(); 
         //Get a connection
         conn = DriverManager.getConnection(dbURL, dbUser, dbUserPassword); 
+        */
+                
+        Class.forName(dbDriver);
+        conn = DriverManager.getConnection(dbURL, dbUser,dbUserPassword);
+        
+        conn.setAutoCommit(false);
+
+        initDatabase();
     }
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
@@ -41,5 +55,37 @@ public class DatabaseConnection {
         if(conn != null && !conn.isClosed()){
             conn.close();
         }
+    }
+    
+    public static void initDatabase() throws SQLException{
+                
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS Usuario (\n" +
+                        "  id bigint auto_increment primary key,\n" +
+                        "  usuario varchar(35) NOT NULL,\n" +
+                        "  password varchar(35) NOT NULL,\n" +
+                        "  nombre varchar(35) NOT NULL,\n" +
+                        "  apellidos varchar(70) NOT NULL,\n" +
+                        "  correo varchar(150) NOT NULL\n" +
+                        ")");
+        
+        stmt.execute("alter table if exists Usuario ADD CONSTRAINT IF NOT EXISTS usuario_UNIQUE UNIQUE(usuario)");
+        stmt.execute("alter table if exists Usuario ADD CONSTRAINT IF NOT EXISTS correo_UNIQUE UNIQUE(correo)");
+        
+        stmt.execute("CREATE TABLE IF NOT EXISTS videos (\n" +
+                        "  id bigint auto_increment primary key,\n" +
+                        "  titulo varchar(100) NOT NULL,\n" +
+                        "  autor varchar(100) NOT NULL,\n" +
+                        "  fechaCreacion datetime NOT NULL,\n" +
+                        "  duracion time NOT NULL,\n" +
+                        "  reproducciones int(11) NOT NULL,\n" +
+                        "  descripcion varchar(255) NOT NULL,\n" +
+                        "  formato varchar(5) NOT NULL\n" +
+                        ")");
+        
+        //stmt.execute("alter table if exists videos ADD CONSTRAINT IF NOT EXISTS titulo_UNIQUE UNIQUE(titulo)");
+        
+        stmt.close();
+        conn.commit();
     }
 }
--- src/java/videos/VideoDAO.java
+++ src/java/videos/VideoDAO.java
@@ -28,46 +28,56 @@ public class VideoDAO {
     }
         
     public static void register(Video video) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{ 
-        init();
-        String sentence = "insert into videos(titulo, autor, fechaCreacion, duracion, reproducciones, descripcion, formato) " +
-                    "values (?,?,?,?,?,?,?)";
-        stmt = connection.prepareStatement(sentence);
-        stmt.setString(1, video.getTitulo());
-        stmt.setString(2, video.getAutor());
-        stmt.setDate(3, new java.sql.Date(video.getFechaCreacion().getTime()));
-        stmt.setTime(4, video.getDuracion());
-        stmt.setInt(5, video.getReproducciones());
-        stmt.setString(6, video.getDescripcion());
-        stmt.setString(7, video.getFormato());
-        stmt.executeUpdate();
-        stmt.close();
-        disconnect();
+        try{
+            init();
+            String sentence = "insert into videos(titulo, autor, fechaCreacion, duracion, reproducciones, descripcion, formato) " +
+                        "values (?,?,?,?,?,?,?)";
+            stmt = connection.prepareStatement(sentence);
+            stmt.setString(1, video.getTitulo());
+            stmt.setString(2, video.getAutor());
+            stmt.setDate(3, new java.sql.Date(video.getFechaCreacion().getTime()));
+            stmt.setTime(4, video.getDuracion());
+            stmt.setInt(5, video.getReproducciones());
+            stmt.setString(6, video.getDescripcion());
+            stmt.setString(7, video.getFormato());
+            stmt.executeUpdate();
+            stmt.close();
+            connection.commit();
+        }
+        finally{
+            disconnect();
+        }
     }  
     
     public static List<Video> retrieve(Integer offset, Integer numRecords) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
         // TODO : falta per aplicar l'offset i el numRecords 
         List<Video> videos = new ArrayList<Video>();
-        Video video = null;
-        String statement = "select titulo, autor, fechaCreacion, duracion, reproducciones, descripcion, formato from videos";
-        init();
-        stmt = connection.prepareStatement(statement);
-        ResultSet results = stmt.executeQuery();
         
-        while(results.next()){
-            video = new Video(results.getObject("TITULO", String.class),
-                            results.getObject("AUTOR", String.class),
-                            results.getDate("FECHACREACION"),
-                            results.getTime("DURACION"),
-                            results.getObject("REPRODUCCIONES", Integer.class),
-                            results.getObject("DESCRIPCION", String.class),
-                            results.getObject("FORMATO", String.class));
-            
-            videos.add(video);
-        }
+        try{
+            Video video = null;
+            String statement = "select titulo, autor, fechaCreacion, duracion, reproducciones, descripcion, formato from videos";
+            init();
+            stmt = connection.prepareStatement(statement);
+            ResultSet results = stmt.executeQuery();
 
-        results.close();
-        stmt.close();
-        disconnect();
+            while(results.next()){
+                video = new Video(results.getObject("TITULO", String.class),
+                                results.getObject("AUTOR", String.class),
+                                results.getDate("FECHACREACION"),
+                                results.getTime("DURACION"),
+                                results.getObject("REPRODUCCIONES", Integer.class),
+                                results.getObject("DESCRIPCION", String.class),
+                                results.getObject("FORMATO", String.class));
+
+                videos.add(video);
+            }
+
+            results.close();
+            stmt.close();
+        }
+        finally{
+            disconnect();
+        }
         
         if(videos.isEmpty()){
             videos = null;
@@ -77,21 +87,25 @@ public class VideoDAO {
     }
     
     public static boolean videoExists(String titulo) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
-        // TODO : falta per aplicar l'offset i el numRecords 
+
         String statement = "select titulo from videos where titulo = '" + titulo + "'";
         boolean exists = false;
         
-        init();
-        stmt = connection.prepareStatement(statement);
-        ResultSet results = stmt.executeQuery();
-        
-        if(results.next()){
-            exists = true;
-        }
+        try{
+            init();
+            stmt = connection.prepareStatement(statement);
+            ResultSet results = stmt.executeQuery();
 
-        results.close();
-        stmt.close();
-        disconnect();
+            if(results.next()){
+                exists = true;
+            }
+
+            results.close();
+            stmt.close();
+        }
+        finally{
+            disconnect();
+        }
         
         return exists;
     }
